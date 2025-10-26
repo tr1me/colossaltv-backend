b1b5eaa334c4ea2b40b1299fa1a3f0e318aee43d
tr1me <156481481+tr1me@users.noreply.github.com>
2025-10-26 15:23:07 -0500

    Update profileModel.js

--- models/profileModel.js
+++ models/profileModel.js
@@ -1,1 +1,28 @@
+const db = require('../db/database');
 
+exports.getAllProfiles = () => {
+  return new Promise((resolve, reject) => {
+    db.all('SELECT * FROM profiles', [], (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+};
+
+exports.updateStatus = (id, status) => {
+  return new Promise((resolve, reject) => {
+    db.run('UPDATE profiles SET status = ? WHERE id = ?', [status, id], (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+};
+
+exports.rotateApiKey = (newKey) => {
+  return new Promise((resolve, reject) => {
+    db.run('UPDATE api SET key = ?', [newKey], (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+};
