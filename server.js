659b5d869bc4cde61900f3f85c1a0b6d0079d955
tr1me <156481481+tr1me@users.noreply.github.com>
2025-10-26 18:22:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const profileRoutes = require('./routes/profiles');
 
 app.use(express.json());
 app.use('/profiles', profileRoutes);
-
+app.get('/', (req, res) => {
+  res.send('COLOSSALTV backend is live');
+});
 app.listen(3000, () => {
   console.log('COLOSSALTV backend running on port 3000');
 });
