93ef8da59f251f60e665f9980bffd2622f7a8734
tr1me <156481481+tr1me@users.noreply.github.com>
2025-10-26 15:22:33 -0500

    Update profiles.js

--- routes/profiles.js
+++ routes/profiles.js
@@ -1,1 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getProfiles,
+  revokeProfile,
+  restoreProfile,
+  rotateApiKey
+} = require('../controllers/profileController');
 
+router.get('/', getProfiles);
+router.post('/:id/revoke', revokeProfile);
+router.post('/:id/restore', restoreProfile);
+router.post('/rotate-key', rotateApiKey);
+
+module.exports = router;
